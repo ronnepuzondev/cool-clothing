e8d63c662b430b75a258d2b22ef5f75138b56ff1
ronnepuzondev <116023509+ronnepuzondev@users.noreply.github.com>
2023-03-23 14:41:34 -0700

    add incremeent and decrement functionality for checkout items

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -16,13 +16,35 @@ export const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  //find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  )
+  //check if quantity is equal to 1, if it is remove that item from the cart
+    if(existingCartItem.quantity ===1 ) {
+      return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id )
+    }
+  //return back cart item with matching cart item with reduced quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  }
+}
+
+
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     cartItemCount: 0,
-    cartCount: 0
+    removeItemFromCart: () => {},
+    cartCount: 0,
   });
   
   export const CartProvider = ({ children }) => {
@@ -38,14 +60,18 @@ export const CartContext = createContext({
       setCartCount(newCartCount);
     }, [cartItems]);
   
-    const addItemToCart = (product) =>
-      setCartItems(addCartItem(cartItems, product));
+    const addItemToCart = (productToAdd) =>
+      setCartItems(addCartItem(cartItems, productToAdd));
+
+      const removeItemToCart = (cartItemToRemove) =>
+      setCartItems(removeCartItem(cartItems, cartItemToRemove));
   
     const value = {
       isCartOpen,
       setIsCartOpen,
       cartItems,
       addItemToCart,
+      removeItemToCart,
       cartCount,
     };
   
